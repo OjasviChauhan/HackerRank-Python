8b78d915934f7916b175712cf598b5fe6ddbd454
Ojasvi Chauhan <500068394@stu.upes.ac.in>
2020-05-17 12:42:50 +0530

    Update Finding the percentage.py

--- Basic Data Types/Finding the percentage.py
+++ Basic Data Types/Finding the percentage.py
@@ -1,11 +1,11 @@
 n = int(input())
-student_marks = {}
+student_marks = {}              #Creating a empty dictionary
 for i in range(n):
     line = input().split()
     name = line[0]
     marks = line[1:]
     avg = (float(marks[0])+float(marks[1])+float(marks[2]))/3
-    student_marks[name] = format(avg, '.2f')
+    student_marks[name] = format(avg, '.2f')  #here, first converting avg unto 2-decimal number and then storing into it's coresponding key
     
 student = input()
 print(student_marks[student])
